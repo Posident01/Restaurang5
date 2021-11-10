5855e9b0e17cd67136972ff747c7fca9961d438b
User <User@LAPTOP-JSA4PVLN>
2021-11-11 02:50:05 +0700

    Add project files.

---
+++ Restaurang5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using cl;
+
+namespace Compro2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] menu = { "1.Rice Chiken.", "2.Khao mun kai.", "3.Kao ka moo.", "4.Noodle Ped", "5.Pad ka prao." };
+            int[] Price = { 40, 50 };
+            int[] money = new int[2] { 1, 2 };
+            int dish, bath = 0;
+            int order;
+
+            do
+            {
+                Console.WriteLine("Welcome to Restaurant.");
+                Console.WriteLine("1.Rice Chiken 40 Baht \n2.Khao mun gai 50 Baht\n3.Kao pad moo 40 Baht\n4.Noodle Ped 50 Baht\n5.Pad Ka Prao 40 Baht\n6.Exit. ");
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+                Console.Write("How much many do you have: ");
+                money[0] = int.Parse(Console.ReadLine());
+                Console.Write($"{name} please select order: ");
+               
+               
+                order = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{name} have {money[0]} baht.");
+
+                if (order == 1 || order == 3 || order == 5)
+                {
+                    bath = 0;  // Price = 40 Baht
+                }
+
+                else
+                {
+                    bath = 1;  // Price = 50 Baht
+                }
+
+                order--;
+
+                Console.WriteLine("You select {0} this {1} baht.", menu[order], Price[bath]);
+                                           
+                Console.Write("How many dishes would you like to order? ");
+                dish = int.Parse(Console.ReadLine());
+
+                Class1.cal(dish, money[order], name, Price[bath]);
+                        Console.WriteLine("****************************");
+
+  
+     
+                        
+                
+            } while (order != 6);
+        }
+
+    }
+}
---
+++ Restaurang5/Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace cl
+{
+    public class Class1
+    {
+        public static void cal(int dish, int money, string name, int Price)
+        {
+            if (money >= dish * Price)
+            {
+                int total;
+                total = money - dish * Price;
+                Console.WriteLine("{1} Need To Pay {0}", dish * Price, name);
+                Console.WriteLine("{1} pay {0} Baht.", money, name);
+                Console.WriteLine("{1} Get change For {0} Baht.", total, name);
+                Console.WriteLine("{1} Get {0} Thousand banknotes Baht.", total / 1000, name);
+                Console.WriteLine("{1} Get {0} Five Hundreds Banknotes Baht.", total % 1000 / 500, name);
+                Console.WriteLine("{1} Get {0} One Hundreds Banknotes Baht.", total % 1000 % 500 / 100, name);
+                Console.WriteLine("{1} Get {0} Fifty Nanknotes Baht.", total % 1000 % 500 % 100 / 50, name);
+                Console.WriteLine("{1} Get {0} Twenty Banknotes Baht.", total % 1000 % 500 % 100 % 50 / 20, name);
+                Console.WriteLine("{1} Get {0} Ten coin Baht.", total % 1000 % 500 % 100 % 50 % 20 / 10, name);
+                Console.WriteLine("{1} Get {0} Five coin Baht.", total % 1000 % 500 % 100 % 50 % 20 % 10 / 5, name);
+                Console.WriteLine("{1} Get {0} One coin Baht.", total % 1000 % 500 % 100 % 50 % 20 % 10 % 5, name);
+            }
+            else if (money <= dish * Price)
+            {
+                Console.WriteLine("{1} Need To Pay {0}", dish * Price, name);
+                Console.WriteLine("Your want add money again? ");
+                Console.Write("1.Yes.\n2.No.\nYou select 1 or 2: ");
+                int Choseadd = int.Parse(Console.ReadLine());
+                if (Choseadd == 1)
+                {
+                    do
+                    {
+                        Console.Write("How much will you fill: ");
+                        money += Convert.ToInt32(Console.ReadLine());
+                    } while (money < dish * Price);
+                    int total;
+                    total = money - dish * Price;
+                    Console.WriteLine("{1} pay {0} Baht.", money, name);
+                    Console.WriteLine("{1} Get change For {0} Baht.", total, name);
+                    Console.WriteLine("{1} Get {0} Thousand banknotes Baht.", total / 1000, name);
+                    Console.WriteLine("{1} Get {0} Five Hundreds Banknotes Baht.", total % 1000 / 500, name);
+                    Console.WriteLine("{1} Get {0} One Hundreds Banknotes Baht.", total % 1000 % 500 / 100, name);
+                    Console.WriteLine("{1} Get {0} Fifty Nanknotes Baht.", total % 1000 % 500 % 100 / 50, name);
+                    Console.WriteLine("{1} Get {0} Twenty Banknotes Baht.", total % 1000 % 500 % 100 % 50 / 20, name);
+                    Console.WriteLine("{1} Get {0} Ten coin Baht.", total % 1000 % 500 % 100 % 50 % 20 / 10, name);
+                    Console.WriteLine("{1} Get {0} Five coin Baht.", total % 1000 % 500 % 100 % 50 % 20 % 10 / 5, name);
+                    Console.WriteLine("{1} Get {0} One coin Baht.", total % 1000 % 500 % 100 % 50 % 20 % 10 % 5, name);
+                }
+            }
+        }
+    }
+}
