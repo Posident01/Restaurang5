6d33e57f955531bd084d42206d6d1e35727b1bdd
Kittipong <6431503005@lamduan.mfu.ac.th>
2021-11-11 03:53:30 +0700

    SAWATDEE

--- Restaurang5/Program.cs
+++ Restaurang5/Program.cs
@@ -12,9 +12,58 @@ namespace Compro2
             int[] money = new int[2] { 1, 2 };
             int dish, bath = 0;
             int order;
+            
+            
+            // bool success;   // use for no error input
+            // success = Int32.TryParse(Console.ReadLine(), out select);   // input select
 
-            do
+            string[] user = {"Posident","1234"};
+            
+            string enter = ""; // not null
+
+            Console.Write("Please Enter Your ID : ");
+            enter = Console.ReadLine();
+            
+        
+            while(true){
+            
+            if(enter == user[0])
             {
+                break;  // exsit loop
+            }
+
+            else{   // กรอกผิด
+                Console.WriteLine("The ID and password are entered incorrectly. Please enter again ");
+                Console.Write("Please Enter Your ID : ");
+                enter = Console.ReadLine();
+                }
+
+            }
+
+
+            Console.Write("Please Enter Your Password : ");
+            enter = Console.ReadLine();
+
+            while(true){
+            
+            if(enter == user[1])
+            {
+                break;  // exsit loop
+            }
+
+            else{
+                Console.WriteLine("The ID and password are entered incorrectly. Please enter again ");
+                Console.Write("Please Enter Your Password : ");
+                enter = Console.ReadLine();
+                }
+
+            }
+                
+
+                
+            Console.Clear();
+
+          
                 Console.WriteLine("Welcome to Restaurant.");
                 Console.WriteLine("1.Rice Chiken 40 Baht \n2.Khao mun gai 50 Baht\n3.Kao pad moo 40 Baht\n4.Noodle Ped 50 Baht\n5.Pad Ka Prao 40 Baht\n6.Exit. ");
                 Console.Write("Enter your name: ");
@@ -25,6 +74,12 @@ namespace Compro2
                
                
                 order = int.Parse(Console.ReadLine());
+
+                if (order == 6) {
+                    System.Environment.Exit(0);
+                }
+
+
                 Console.WriteLine($"{name} have {money[0]} baht.");
 
                 if (order == 1 || order == 3 || order == 5)
@@ -45,13 +100,14 @@ namespace Compro2
                 dish = int.Parse(Console.ReadLine());
 
                 Class1.cal(dish, money[order], name, Price[bath]);
+
                         Console.WriteLine("****************************");
 
-  
-     
-                        
-                
-            } while (order != 6);
+            
+
+
+
+
         }
 
     }
